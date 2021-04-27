2c858deda83b2bca078c00caeca459edc22e56c0
JoyMbugua <wacumbugua@gmail.com>
2021-04-27 07:36:11 +0300

    created initial app

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+def create_app(config_name):
+
+    app = Flask(__name__)
+    
+    bootstrap.init_app(app)
+
+    # blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+
+    return app
---
+++ manage.py
@@ -0,0 +1,14 @@
+from app import create_app
+from flask_script import Manager, Server
+
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server', Server)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app)
+
+if __name__ == '__main__':
+    manager.run()
